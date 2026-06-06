95392e2100b9b7c8eeef6a91e2654e9fac023978
naveendanj <naveenhettiwaththa@gmail.com>
2026-06-06 19:54:52 +0530

    feat: added latest changes

--- src/backend/asm_builder.zig
+++ src/backend/asm_builder.zig
@@ -6,27 +6,31 @@ pub const AsmBuilder = struct {
     instructions: std.ArrayList(Instruction),
     buffer: std.ArrayList(u8),
 
-    pub fn init(allocator: std.mem.Allocator) AsmBuilder {
-        return .{ .allocator = allocator, .instructions = std.ArrayList(Instruction).init(allocator), .buffer = std.ArrayList(u8).initCapacity(allocator, 0) };
+    pub fn init(allocator: std.mem.Allocator) !AsmBuilder {
+        return .{ .allocator = allocator, .instructions = try std.ArrayList(Instruction).initCapacity(allocator, 0), .buffer = try std.ArrayList(u8).initCapacity(allocator, 0) };
     }
 
     pub fn deinit(self: *AsmBuilder) void {
-        self.instructions.deinit();
-        self.buffer.deinit();
+        self.instructions.deinit(self.allocator);
+        self.buffer.deinit(self.allocator);
     }
 
-    pub fn loadInstruction(self: *AsmBuilder, instructions: []Instruction) !void {
+    pub fn LoadInstructions(self: *AsmBuilder, instructions: []Instruction) !void {
         for (instructions) |instruction| {
             try self.instructions.append(self.allocator, instruction);
         }
     }
 
-    pub fn emit(self: *AsmBuilder, fmt: []const u8, args: anytype) !void {
-        try self.buffer.writer().print(fmt, args);
-        try self.buffer.append(self.allocator, "\n");
+    pub fn emit(self: *AsmBuilder, comptime fmt: []const u8, args: anytype) !void {
+        const line = try std.fmt.allocPrint(self.allocator, fmt, args);
+        defer self.allocator.free(line);
+
+        try self.buffer.appendSlice(self.allocator, line);
+        try self.buffer.append(self.allocator, '\n');
     }
 
-    pub fn toOwnedSlice(self: *AsmBuilder) []const u8 {
-        return self.buffer.toOwnedSlice();
+    pub fn toOwnedSlice(self: *AsmBuilder) ![]const u8 {
+        const result = try self.buffer.toOwnedSlice(self.allocator);
+        return result;
     }
 };
