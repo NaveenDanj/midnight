d17dbb91be47f8dadff9bd7c1458e1d0304db207
naveendanj <naveenhettiwaththa@gmail.com>
2026-07-21 04:48:00 +0530

    feat: implemented print emiiter

--- src/backend/llvm/llvm_backend.zig
+++ src/backend/llvm/llvm_backend.zig
@@ -8,6 +8,7 @@ const Value = @import("../../ir/ir.zig").Value;
 const realPredicate = @import("./lib/predicates.zig").realPredicate;
 const intPredicate = @import("./lib/predicates.zig").intPredicate;
 const lowerBinaryOp = @import("./lib/bin_op_emitter.zig").lowerBinaryOp;
+const lowerPrintCall = @import("./lib/print_emitter.zig").lowerPrintCall;
 
 pub const LLVMBackendError = error{
     MissingResolvedType,
@@ -193,7 +194,7 @@ pub const LLVMBackend = struct {
                 _ = c.LLVMBuildRet(self.builder, try self.valueRef(inst.value, typ));
                 self.current_block_terminated = true;
             },
-            .PrintCall => |inst| try self.lowerPrintCall(inst.value, inst.resolvedType orelse return LLVMBackendError.MissingResolvedType),
+            .PrintCall => |inst| try lowerPrintCall(inst.value, inst.resolvedType orelse return LLVMBackendError.MissingResolvedType),
             .FunctionIR,
             .FunctionCall,
             .ParamBind,
@@ -218,37 +219,6 @@ pub const LLVMBackend = struct {
         var puts_params = [_]c.LLVMTypeRef{ptr_type};
         self.puts_type = c.LLVMFunctionType(i32_type, &puts_params, puts_params.len, 0);
         self.puts_function = c.LLVMAddFunction(self.module, "puts", self.puts_type);
-    }
-
-    fn lowerPrintCall(self: *LLVMBackend, value: Value, typ: Type) !void {
-        switch (typ.kind) {
-            .INT => {
-                const format = c.LLVMBuildGlobalStringPtr(self.builder, "%lld", try self.nextName("fmt.int"));
-                var args = [_]c.LLVMValueRef{ format, try self.valueRef(value, typ) };
-                _ = c.LLVMBuildCall2(self.builder, self.printf_type, self.printf_function, &args, args.len, "");
-            },
-            .BOOL => {
-                const format = c.LLVMBuildGlobalStringPtr(self.builder, "%lld", try self.nextName("fmt.bool"));
-                const bool_as_int = c.LLVMBuildZExt(
-                    self.builder,
-                    try self.valueRef(value, typ),
-                    c.LLVMInt64TypeInContext(self.context),
-                    try self.nextName("bool.print"),
-                );
-                var args = [_]c.LLVMValueRef{ format, bool_as_int };
-                _ = c.LLVMBuildCall2(self.builder, self.printf_type, self.printf_function, &args, args.len, "");
-            },
-            .FLOAT => {
-                const format = c.LLVMBuildGlobalStringPtr(self.builder, "%f", try self.nextName("fmt.float"));
-                var args = [_]c.LLVMValueRef{ format, try self.valueRef(value, typ) };
-                _ = c.LLVMBuildCall2(self.builder, self.printf_type, self.printf_function, &args, args.len, "");
-            },
-            .STRING => {
-                var args = [_]c.LLVMValueRef{try self.valueRef(value, typ)};
-                _ = c.LLVMBuildCall2(self.builder, self.puts_type, self.puts_function, &args, args.len, "");
-            },
-            else => return LLVMBackendError.UnsupportedType,
-        }
     }
 
     fn ensureVariable(self: *LLVMBackend, name: []const u8, typ: Type) !VariableRef {
